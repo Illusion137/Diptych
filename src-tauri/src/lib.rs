accacf0f0cc6474d0ab861b7da35369e96dc4802
Illusion137 <raygoza.d@hotmail.com>
2026-03-24 23:04:52 -0700

    feat: update for building + signing macos + tauri updater

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -19,6 +19,7 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .invoke_handler(tauri::generate_handler![read_file, write_file])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
